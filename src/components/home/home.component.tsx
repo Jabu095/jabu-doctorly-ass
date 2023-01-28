7c86b15fef1ce309be1026b278b1ec61642d316d
Jabulani Madzivadondo <jabulani.madzivadondo@twipemobile.com>
2023-01-28 19:44:22 +0200

    adding simple search

--- src/components/home/home.component.tsx
+++ src/components/home/home.component.tsx
@@ -1,27 +1,46 @@
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Fuse from 'fuse.js';
 import { SearchResult } from '../../types';
 import { data } from '../../utils/data';
 
   
 const searchData = [...data] as unknown as SearchResult[];
-  console.log("🚀 ~ file: home.component.tsx:9 ~ searchData", searchData)
-  
-  const options = {
+
+const options = {
+    includeScore: true,
     keys: ['name'],
-  };
+};
   
 const fuse = new Fuse(searchData, options);
-
 const HomeComponent = () => {
-    useEffect(() => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
 
-    },[])
+  const handleSearch = (searchTerm: string) => {
+    setSearchTerm(searchTerm);
+    const _searchResults = fuse.search(searchTerm).map(value => value.item);
+    if(_searchResults.length > 0){
+        setSearchResults(_searchResults);
+    }
+  };
 
     return (
-        <div>
-        </div>
+    <div>
+      <div>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={event => handleSearch(event.target.value)}
+          placeholder="Search..."
+        />
+      </div>
+      <div>
+        {searchResults.map((result,i) => (
+          <div key={i}>{result.name}</div>
+        ))}
+      </div>
+    </div>
     )
 }
 
