df2cf80c957c2a5d768e7ffb42ce2665b827e928
Jabulani Madzivadondo <jabulani.madzivadondo@twipemobile.com>
2023-01-28 21:04:36 +0200

    adding snap shots

--- src/components/home/__tests__/home.test.tsx
+++ src/components/home/__tests__/home.test.tsx
@@ -1,8 +1,16 @@
 import { render , screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import HomeComponent from '../home.component';
 
 test('should get home componet', () => {
     render(<HomeComponent />);
     const paragraphElement = screen.getByText('Fuzzy search');
     expect(paragraphElement).toBeInTheDocument();
+})
+
+test('renders correctly', () => {
+    const tree = renderer
+    .create(<HomeComponent />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 })
