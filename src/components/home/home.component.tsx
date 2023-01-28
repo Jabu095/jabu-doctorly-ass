eb2ad6104cf2df40a39a6aee238897e222729a35
Jabulani Madzivadondo <jabulani.madzivadondo@twipemobile.com>
2023-01-28 20:26:57 +0200

    feat: adding keyboard events

--- src/components/home/home.component.tsx
+++ src/components/home/home.component.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Fuse from 'fuse.js';
 import { SearchResult } from '../../types';
 import { data } from '../../utils/data';
@@ -25,6 +25,8 @@ const HomeComponent = () => {
     const [showResults, setShowResults] = useState(false);
     const [selectedItem, setSelectedItem] = useState<string | undefined>(undefined);
     const [previousSearchTerm, setPreviousSearchTerm] = useState('');
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+    const inputRef = useRef<HTMLInputElement | null>(null);
 
     const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -57,12 +59,37 @@ const HomeComponent = () => {
         setSearchTerm(previousSearchTerm);
     };
     
+    const handleKeyDown = (event:any) => {
+        if (searchResults.length === 0) return;
+        if (event.keyCode === 38) {
+          // arrow up
+          event.preventDefault();
+          setSelectedIndex(Math.max(selectedIndex - 1, 0));
+        } else if (event.keyCode === 40) {
+          // arrow down
+          event.preventDefault();
+          setSelectedIndex(Math.min(selectedIndex + 1, searchResults.length - 1));
+        } else if (event.keyCode === 13) {
+          // enter
+          event.preventDefault();
+          const selectedResult = searchResults[selectedIndex];
+          setSearchTerm(selectedResult.name);
+          setSearchResults([]);
+        }
+    };
+
+    useEffect(() => {
+        inputRef.current?.addEventListener('keydown', handleKeyDown);
+        return () => inputRef.current?.removeEventListener('keydown', handleKeyDown);
+    }, [inputRef]);
+    
     return (
         <div>
             <form onSubmit={handleSearch}>
                 <label>
                     Search:
                     <input
+                        ref={inputRef}
                         type="text"
                         value={searchTerm}
                         onChange={event => setSearchTerm(event.target.value)}
